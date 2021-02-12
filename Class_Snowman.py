65ccbf65353710e707ced23d868fd587d9beab5c
Fargo <fixlab.od@gmail.com>
2021-02-12 13:21:10 +0200

    Different class

--- Class_Snowman.py
+++ Class_Snowman.py
@@ -5,21 +5,49 @@ import random as rd
 # Initialize the game engine
 pg.init()
 
-# All varibals
-class All_Varibals:
 
-    def __init__(self):
+class  Create_Game_Space:
     
+    def __init__(self):
+
         #Definind the colors we will use in RGB format
         self.color_black = (0, 0, 0)
-        self.color_white = (255, 255, 255)
-        self.color_gray = (230, 200, 200)
-        
+
         # Size visible window
         self.size_screen = [600, 500]
+
+        # Create a visible space
+        self.screen = pg.display.set_mode(self.size_screen)
+          
+        pg.display.set_caption("--- My Snowman ---")
+
+        self.screen.fill(self.color_black)
+
+
+# Create  ground space
+class Ground(Create_Game_Space):
+
+    def __init__(self):
         
+        # Definind the colors we will use in RGB format
+        self.color_white = (255, 255, 255)
+
         # Coordinates and dimensions ground
         self.ground = [0, 360, 600, 200]
+
+    def draw(self):
+
+        Create_Game_Space.__init__(self)
+
+        pg.draw.rect(self.screen, self.color_white, self.ground)
+       
+
+class Snowman(Ground, Create_Game_Space):
+
+    def __init__(self):
+        
+        #Definind the colors we will use in RGB format
+        self.color_gray = (230, 200, 200)
 
         # Coordinates and dimensions snowman
         self.size_lower_ball = 80
@@ -29,114 +57,143 @@ class All_Varibals:
         self.coord_lower_ball = [300, 400]
         self.coord_middle_ball = [300, 270]
         self.coord_ball_head = [300, 180]
-
-        #Defining other objects
-        self.done = False
-
-# Event handling. Closing the window by clicking on the "Close window" button
-class Check_Events(All_Varibals):
-
-    def _init__(self):
-        pass
     
-    # Constantly check for pressing the button to close the window.
-    def check_event(self):
 
-        all_var = All_Varibals()
-                   
-        while not all_var.__init__.done:
-            for self.event in pg.event.get():
-                if self.event.type == pg.QUIT:
-                    all_var.__init__.done = True
+    def draw(self):
 
-        # Number of events per second
-        clock = pg.time.Clock()
+        Create_Game_Space.__init__(self)
+        Ground.__init__(self)
 
-        clock.tick(20)
+        Ground.draw(self)
+       
+        pg.draw.circle(self.screen, self.color_gray, self.coord_lower_ball, self.size_lower_ball)
+        
+        pg.draw.circle(self.screen, self.color_gray, self.coord_middle_ball, self.size_middle_ball)
+        
+        pg.draw.circle(self.screen, self.color_gray, self.coord_ball_head, self.size_ball_head)
 
-# Create  ground space
-class Ground():
+
+class Moon(Snowman, Create_Game_Space):
 
     def __init__(self):
 
-        pass
+        # #Definind the colors we will use in RGB format
+        # self.color_gray = (230, 200, 200)
 
+        # Coordinates and dimensions snowman
+        self.size_moon1 = [500, 60]
+        self.size_moon2 = [475, 60]
+        self.diameter_moon = 40
 
-    def ground(self):
+    def draw(self):
+
+        Create_Game_Space.__init__(self)
+        Snowman.__init__(self)
+            
         
-        all_var = All_Varibals()
-
-        self.screen = pg.display.set_mode(all_var.size_screen)
-
-        pg.draw.rect(self.screen, all_var.color_white, all_var.ground)
-
-        
-        pg.display.flip()   
+        Snowman.draw(self)
 
 
-# class Snowman(Ground):
+        pg.draw.circle(self.screen, self.color_gray, self.size_moon1, self.diameter_moon)
+        pg.draw.circle(self.screen, self.color_black, self.size_moon2, self.diameter_moon)
 
+
+# # Event handling. Closing the window by clicking on the "Close window" button
+# class Check_Events:
+
+#     def __init__(self):
+
+#         self.done = False
+
+#         self.clock = pg.time.Clock()
     
-#     def snowman(self):
+    # Constantly check for pressing the button to close the window.
+    # def check_event(self):
+       
 
-#         all_var = All_Varibals()
+    # # while not self.done:
+    #     for self.event in pg.event.get():
+    #         if self.event.type == pg.QUIT:
+    #             self.done = True        
 
+    #         # Number of events per second
+    #         self.clock.tick(20)
+
+
+
+class Snow(Create_Game_Space):
+
+    def __init__(self):
+
+        self.clock = pg.time.Clock()
+        self.snow_list = []   
+        self.color_white = (255, 255, 255)
+
+        self.done = False
+       
+    def draw(self):
+
+        Create_Game_Space.__init__(self)
+        Ground.__init__(self)  
+        Snowman.__init__(self)
+        Moon.__init__(self)   
+
+               
+
+        for self.i in range(80):
+            self.x = rd.randrange(0, 600)
+            self.y = rd.randrange(0, 500)
+
+            self.snow_list.append([self.x, self.y])
+
+
+        while not self.done:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.done = True
+            
+            self.screen.fill(self.color_black)
+            
+
+            Ground.draw(self)
+            Snowman.draw(self)
+            Moon.draw(self)     
+                
+
+            for self.i in range(len(self.snow_list)):
+
+                pg.draw.circle(self.screen, self.color_white, self.snow_list[self.i], 2)
+
+                self.snow_list[self.i][1] += 1
+
+                if  self.snow_list[self.i][1] > 500:
+                    self.y = rd.randrange(-50, -10)
+                    self.snow_list[self.i][1] = self.y
+                    self.x = rd.randrange(0, 600)
+                    self.snow_list[self.i][0] = self.x
+
+            pg.display.flip()
+            
+            self.clock.tick(20)
         
+                  
+class Manager:      
+
+    def launch(self):      
+
+        # self.win = Create_Game_Space()
+        # self.grd = Ground()
+        # self.sn = Snowman()
+        # self.mn = Moon()
+        self.snow = Snow()
 
 
-#         pg.draw.circle(pg.display.set_mode(all_var.size_screen), all_var.color_gray, all_var.coord_lower_ball, all_var.size_lower_ball)
-#         pg.display.flip() 
-#         # pg.draw.circle(pg.display.set_mode(all_var.size_screen), all_var.color_gray, all_var.coord_middle_ball, all_var.size_middle_ball)
+        # self.win.__init__()
+        # self.grd.draw()
+        # self.sn.draw()
+        # self.mn.draw()
+        self.snow.draw()
         
-#         # pg.draw.circle(pg.display.set_mode(all_var.size_screen), all_var.color_gray, all_var.coord_ball_head, all_var.size_ball_head)
-
-#         # pg.display.flip()  
-
-# Creating all space
-class  Create_Game_Space(Ground):
-   
-    # Create a visible space
-    def draw_window(self):
-
-        all_var = All_Varibals()
-
-        c_e = Check_Events()
-
-        grd = Ground()
-
-        # s_n = Snowman()
-
         
-        self.screen = pg.display.set_mode(all_var.size_screen)
-
-        pg.display.set_caption("--- My Snowman ---")
-
-        self.screen.fill(all_var.color_black)
-
-        # Create ground
-        grd.ground()
-
-        # Fix Mi
-        
-
-        # s_n.ground()
-
-        # s_n.snowman()
-
-        # Call call event handler. Closing the window by clicking on the "Close window" button
-        c_e.check_event()
-
-# Launch of the whole project
-class Launch:
-
-    # Create game space
-    c_g_s = Create_Game_Space()
-
-    c_g_s.draw_window()
-
-
-# Launch whole project
-run = Launch()
-
-# Exit the game engine
-pg.quit()
+run = Manager()
+run.launch()
--- Test.py
+++ Test.py
@@ -5,162 +5,164 @@ import random as rd
 # Initialize the game engine
 pg.init()
 
-# All varibals
-class All_Varibals:
 
-    def __init__(self):
-        pass
-    
-    #Definind the colors we will use in RGB format
-    color_black = (0, 0, 0)
-    color_white = (255, 255, 255)
-    color_gray = (230, 200, 200)
-    
-    # Size visible window
-    size_screen = [600, 500]
-    screen = pg.display.set_mode(size_screen)
-    
-    # Coordinates and dimensions ground
-    ground = [0, 360, 600, 200]
-
-    # Coordinates and dimensions snowman
-    size_lower_ball = 80
-    size_middle_ball = 60
-    size_ball_head = 40
-
-    coord_lower_ball = [300, 400]
-    coord_middle_ball = [300, 270]
-    coord_ball_head = [300, 180]
-
-    # Coordinates and dimensions snowman
-    size_moon1 = [500, 60]
-    size_moon2 = [475, 60]
-    diameter_moon = 40
-
-    #Defining other objects
-    done = False
-
-class  Create_Game_Space(All_Varibals):
+class  Create_Game_Space:
     
     def __init__(self):
-        pass
 
-    # Create a visible space
-    def draw_window(self):
-        
-        
-        screen = pg.display.set_mode(All_Varibals.size_screen)
+        #Definind the colors we will use in RGB format
+        self.color_black = (0, 0, 0)
 
+        # Size visible window
+        self.size_screen = [600, 500]
+
+        # Create a visible space
+        self.screen = pg.display.set_mode(self.size_screen)
+          
         pg.display.set_caption("--- My Snowman ---")
 
-        screen.fill(All_Varibals.color_black)
-
-
-# Event handling. Closing the window by clicking on the "Close window" button
-class Check_Events(All_Varibals):
-
-    def _init__(self):
-        pass
-    
-
-    # Constantly check for pressing the button to close the window.
-    def check_event(self):
-
-        self.clock = pg.time.Clock()
-                           
-        
-        while not All_Varibals.done:
-            for self.event in pg.event.get():
-                if self.event.type == pg.QUIT:
-                    All_Varibals.done = True        
-
-            # Number of events per second
-            self.clock.tick(20)
+        self.screen.fill(self.color_black)
 
 
 # Create  ground space
-class Ground(All_Varibals):
+class Ground(Create_Game_Space):
 
     def __init__(self):
+        
+        # Definind the colors we will use in RGB format
+        self.color_white = (255, 255, 255)
 
-        pass
-
+        # Coordinates and dimensions ground
+        self.ground = [0, 360, 600, 200]
 
     def draw(self):
-        
-                
-        pg.draw.rect(All_Varibals.screen, All_Varibals.color_white, All_Varibals.ground)
 
+        Create_Game_Space.__init__(self)
 
+        pg.draw.rect(self.screen, self.color_white, self.ground)
        
 
-
-class Snowman(All_Varibals):
+class Snowman(Ground, Create_Game_Space):
 
     def __init__(self):
-        pass
+        
+        #Definind the colors we will use in RGB format
+        self.color_gray = (230, 200, 200)
+
+        # Coordinates and dimensions snowman
+        self.size_lower_ball = 80
+        self.size_middle_ball = 60
+        self.size_ball_head = 40
+
+        self.coord_lower_ball = [300, 400]
+        self.coord_middle_ball = [300, 270]
+        self.coord_ball_head = [300, 180]
     
 
     def draw(self):
+
+        Create_Game_Space.__init__(self)
+        Ground.__init__(self)
+
+        Ground.draw(self)
        
-        pg.draw.circle(All_Varibals.screen, All_Varibals.color_gray, All_Varibals.coord_lower_ball, All_Varibals.size_lower_ball)
+        pg.draw.circle(self.screen, self.color_gray, self.coord_lower_ball, self.size_lower_ball)
         
-        pg.draw.circle(All_Varibals.screen, All_Varibals.color_gray, All_Varibals.coord_middle_ball, All_Varibals.size_middle_ball)
+        pg.draw.circle(self.screen, self.color_gray, self.coord_middle_ball, self.size_middle_ball)
         
-        pg.draw.circle(All_Varibals.screen, All_Varibals.color_gray, All_Varibals.coord_ball_head, All_Varibals.size_ball_head)
+        pg.draw.circle(self.screen, self.color_gray, self.coord_ball_head, self.size_ball_head)
 
 
-class Moon(All_Varibals):
+class Moon(Snowman, Create_Game_Space):
 
     def __init__(self):
-        pass
+
+        # #Definind the colors we will use in RGB format
+        # self.color_gray = (230, 200, 200)
+
+        # Coordinates and dimensions snowman
+        self.size_moon1 = [500, 60]
+        self.size_moon2 = [475, 60]
+        self.diameter_moon = 40
 
     def draw(self):
-        pg.draw.circle(All_Varibals.screen, All_Varibals.color_gray, All_Varibals.size_moon1, All_Varibals.diameter_moon)
-        pg.draw.circle(All_Varibals.screen, All_Varibals.color_black, All_Varibals.size_moon2, All_Varibals.diameter_moon)
+
+        Create_Game_Space.__init__(self)
+        Snowman.__init__(self)
+            
+        
+        Snowman.draw(self)
 
 
-class Snow(Ground, Moon, Snowman, All_Varibals):
+        pg.draw.circle(self.screen, self.color_gray, self.size_moon1, self.diameter_moon)
+        pg.draw.circle(self.screen, self.color_black, self.size_moon2, self.diameter_moon)
+
+
+# # Event handling. Closing the window by clicking on the "Close window" button
+# class Check_Events:
+
+#     def __init__(self):
+
+#         self.done = False
+
+#         self.clock = pg.time.Clock()
+    
+    # Constantly check for pressing the button to close the window.
+    # def check_event(self):
+       
+
+    # # while not self.done:
+    #     for self.event in pg.event.get():
+    #         if self.event.type == pg.QUIT:
+    #             self.done = True        
+
+    #         # Number of events per second
+    #         self.clock.tick(20)
+
+
+
+class Snow(Create_Game_Space):
 
     def __init__(self):
-        pass
 
-    def draw(self):
-        
-        
         self.clock = pg.time.Clock()
-        self.snow_list = []            
-        
+        self.snow_list = []   
+        self.color_white = (255, 255, 255)
 
-        for self.i in range(50):
+        self.done = False
+       
+    def draw(self):
+
+        Create_Game_Space.__init__(self)
+        Ground.__init__(self)  
+        Snowman.__init__(self)
+        Moon.__init__(self)   
+
+               
+
+        for self.i in range(80):
             self.x = rd.randrange(0, 600)
             self.y = rd.randrange(0, 500)
 
             self.snow_list.append([self.x, self.y])
 
-        
 
-        while not All_Varibals.done:
-            for self.event in pg.event.get():
-                if self.event.type == pg.QUIT:
-                    All_Varibals.done = True 
-                     
+        while not self.done:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.done = True
+            
+            self.screen.fill(self.color_black)
             
 
-            All_Varibals.screen.fill(All_Varibals.color_black)
-
-            g = Ground()
-            g.draw()
-
-            s = Snowman()
-            s.draw()
-
-            m = Moon()
-            m.draw()        
+            Ground.draw(self)
+            Snowman.draw(self)
+            Moon.draw(self)     
+                
 
             for self.i in range(len(self.snow_list)):
 
-                pg.draw.circle(All_Varibals.screen, All_Varibals.color_white, self.snow_list[self.i], 2)
+                pg.draw.circle(self.screen, self.color_white, self.snow_list[self.i], 2)
 
                 self.snow_list[self.i][1] += 1
 
@@ -171,39 +173,27 @@ class Snow(Ground, Moon, Snowman, All_Varibals):
                     self.snow_list[self.i][0] = self.x
 
             pg.display.flip()
-        
-        
-
-        # Number of events per second
-        # clock = pg.time.Clock()
-        
+            
             self.clock.tick(20)
+        
+                  
+class Manager:      
 
-        
-class Manager(Snow):      
+    def launch(self):      
 
-    def launch(self):
-        win = Create_Game_Space()
-        # c_e = Check_Events()
-        grd = Ground()
-        sn = Snowman()
-        mn = Moon()
-        snow = Snow()
+        # self.win = Create_Game_Space()
+        # self.grd = Ground()
+        # self.sn = Snowman()
+        # self.mn = Moon()
+        self.snow = Snow()
 
 
-        win.draw_window()
-       
-        grd.draw()
-        sn.draw()
-        mn.draw()
-        snow.draw()
-        pg.display.flip()  
+        # self.win.__init__()
+        # self.grd.draw()
+        # self.sn.draw()
+        # self.mn.draw()
+        self.snow.draw()
         
         
-        
-        # c_e.check_event()
-        
-        
-
 run = Manager()
 run.launch()
