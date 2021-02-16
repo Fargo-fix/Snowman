983a5fd8876d5c1a9d367f04a0c5598e8a0d48b1
Fargo <fixlab.od@gmail.com>
2021-02-16 09:22:45 +0200

    Draw nose and buttons

--- Class_Snowman.py
+++ Class_Snowman.py
@@ -48,37 +48,72 @@ class Snowman(Ground, Create_Game_Space):
         
         #Definind the colors we will use in RGB format
         self.color_gray = (230, 200, 200)
+        self.color_red = (209, 25, 25)
+        self.color_green = (0, 255, 0)
 
-        # Coordinates and dimensions snowman
-        self.size_lower_ball = 80
-        self.size_middle_ball = 60
-        self.size_ball_head = 40
+        # Coordinates and dimensions body snowman
+        self.size_lower_ball = 80 # starting value
+        self.size_middle_ball = self.size_lower_ball - 20
+        self.size_ball_head = self.size_lower_ball - 40
 
-        self.coord_lower_ball = [300, 400]
-        self.coord_middle_ball = [300, 270]
-        self.coord_ball_head = [300, 180]
-    
+        self.x = 300
+        self.y = 400
+        self.coord_lower_ball = [self.x, self.y]
+        self.coord_middle_ball = [self.x, self.y - 130]
+        self.coord_ball_head = [self.x, self.y - 220]
+
+        # Coordinates and dimensions eyes snowman
+        self.size_eyes = self.size_lower_ball - 74
+
+        self.coord_right_eye = [self.x - 15, self.y - 230]
+        self.coord_left_eye = [self.x + 15, self.y - 230]
+
+        # Coordinates and dimensions buttons
+        self.size_button = self.size_lower_ball - 76
+
+        self.coord_button1 = [self.x, self.y - 20]
+        self.coord_button2 = [self.x, self.y - 60]
+        self.coord_button3 = [self.x, self.y - 100]
+        self.coord_button4 = [self.x, self.y - 140]
+
+        # Coordinates and dimensions nose
+        self.size_nose = self.size_lower_ball - 76
+        self.coord_nose = [self.x, self.y - 215]
+      
+        # Coordinates and dimensions mouth
+
 
     def draw(self):
 
         Create_Game_Space.__init__(self)
         Ground.__init__(self)
-
         Ground.draw(self)
-       
+
+        # Draw body snowman
         pg.draw.circle(self.screen, self.color_gray, self.coord_lower_ball, self.size_lower_ball)
-        
         pg.draw.circle(self.screen, self.color_gray, self.coord_middle_ball, self.size_middle_ball)
-        
         pg.draw.circle(self.screen, self.color_gray, self.coord_ball_head, self.size_ball_head)
 
+        # Draw eyes
+        pg.draw.circle(self.screen, self.color_black, self.coord_right_eye, self.size_eyes)
+        pg.draw.circle(self.screen, self.color_black, self.coord_left_eye, self.size_eyes)
+
+        # Draw buttons
+        pg.draw.circle(self.screen, self.color_black, self.coord_button1, self.size_button)
+        pg.draw.circle(self.screen, self.color_black, self.coord_button2, self.size_button)
+        pg.draw.circle(self.screen, self.color_black, self.coord_button3, self.size_button)
+        pg.draw.circle(self.screen, self.color_black, self.coord_button4, self.size_button)
+
+        # Draw nose
+        pg.draw.circle(self.screen, self.color_red, self.coord_nose, self.size_nose)
+
+        # Draw mouth
+        pg.draw.circle(self.screen, self.color_green, [250, 250], 40, 20)
+        # pygame.draw.circle(screen, BLACK, [250, 250], 40, 10, draw_bottom_right=True
 
 class Moon(Snowman, Create_Game_Space):
 
     def __init__(self):
-
-        # #Definind the colors we will use in RGB format
-        # self.color_gray = (230, 200, 200)
 
         # Coordinates and dimensions snowman
         self.size_moon1 = [500, 60]
@@ -98,29 +133,6 @@ class Moon(Snowman, Create_Game_Space):
         pg.draw.circle(self.screen, self.color_black, self.size_moon2, self.diameter_moon)
 
 
-# # Event handling. Closing the window by clicking on the "Close window" button
-# class Check_Events:
-
-#     def __init__(self):
-
-#         self.done = False
-
-#         self.clock = pg.time.Clock()
-    
-    # Constantly check for pressing the button to close the window.
-    # def check_event(self):
-       
-
-    # # while not self.done:
-    #     for self.event in pg.event.get():
-    #         if self.event.type == pg.QUIT:
-    #             self.done = True        
-
-    #         # Number of events per second
-    #         self.clock.tick(20)
-
-
-
 class Snow(Create_Game_Space):
 
     def __init__(self):
@@ -138,14 +150,11 @@ class Snow(Create_Game_Space):
         Snowman.__init__(self)
         Moon.__init__(self)   
 
-               
-
         for self.i in range(80):
             self.x = rd.randrange(0, 600)
             self.y = rd.randrange(0, 500)
 
             self.snow_list.append([self.x, self.y])
-
 
         while not self.done:
             for event in pg.event.get():
@@ -154,12 +163,10 @@ class Snow(Create_Game_Space):
             
             self.screen.fill(self.color_black)
             
-
             Ground.draw(self)
             Snowman.draw(self)
             Moon.draw(self)     
                 
-
             for self.i in range(len(self.snow_list)):
 
                 pg.draw.circle(self.screen, self.color_white, self.snow_list[self.i], 2)
@@ -173,7 +180,6 @@ class Snow(Create_Game_Space):
                     self.snow_list[self.i][0] = self.x
 
             pg.display.flip()
-            
             self.clock.tick(20)
         
                   
@@ -181,17 +187,7 @@ class Manager:
 
     def launch(self):      
 
-        # self.win = Create_Game_Space()
-        # self.grd = Ground()
-        # self.sn = Snowman()
-        # self.mn = Moon()
         self.snow = Snow()
-
-
-        # self.win.__init__()
-        # self.grd.draw()
-        # self.sn.draw()
-        # self.mn.draw()
         self.snow.draw()
         
         
